6ed6ddc9e14086becbc4a9002b4800c17a16fb6b
Calvin Kusek <ckusek2@student13.cse.nd.edu>
2021-02-15 17:04:19 -0500

    Progress so far

--- project1/textdetect.c
+++ project1/textdetect.c
@@ -3,6 +3,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <errno.h>
+#include <string.h>
 
 int main(int argc, char *argv[]){
 	
@@ -10,24 +12,25 @@ int main(int argc, char *argv[]){
 	if(argc != 3){	// Making sure there is the correct number of args
 		printf("Incorrect number of arguments...\n");
 		printf("Usage: textdetect InputFile Threshold\n");
-		return 1;	// exit
+		exit(1);	// exit
 	}
 
 	int thresh = atoi(argv[2]);	// Inputted threshold
 	if(thresh < 0){	// Making sure threshold is positive
 		printf("Threshold cannot be negative\n");
-		return 1;	// exit
+		exit(1);	// exit
 	}
 
 	// Opening file for reading
 	char *filename = argv[1];
-	FILE *file = fopen(filename, "r");
+	int fd = open(filename, O_RDONLY,0);
 
 	// Testing to make sure filename is an actual file
-	if(file == NULL){
-		printf("Couldn't open %s: No such file\n", filename);
+	if(fd < 0){
+		printf("Unable to open %s: %s\n", filename, strerror(errno));
+		exit(1);	// exit
 	}
 
-
+	
 	
 }
