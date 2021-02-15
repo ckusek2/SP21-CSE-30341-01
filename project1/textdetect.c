0af7f4ddc5df8d999944202127bb7868d281acf2
Calvin Kusek <ckusek2@student13.cse.nd.edu>
2021-02-15 16:30:37 -0500

    Added input checks

--- project1/textdetect.c
+++ project1/textdetect.c
@@ -4,6 +4,20 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-int main(){
+int main(int argc, char *argv[]){
+	
+	// Verify input
+	if(argc != 3){	// Making sure there is the correct number of args
+		printf("Incorrect number of arguments...\n");
+		printf("Usage: textdetect InputFile Threshold\n");
+		return 1;	// exit
+	}
+
+	if(atoi(argv[2]) < 0){	// Making sure threshold is positive
+		printf("Threshold cannot be negative\n");
+		return 1;	// exit
+	}
+
+
 	
 }
