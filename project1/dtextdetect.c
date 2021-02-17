bb698f2fde62b5f783d4313564576e8239f869b4
Rafael Mendizabal <rmendiza@student13.cse.nd.edu>
2021-02-16 20:48:58 -0500

    dtextdetect.c changes

--- project1/dtextdetect.c
+++ project1/dtextdetect.c
@@ -6,14 +6,21 @@
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <dirent.h>
 
 // Initialize directory, byte, and file counting variables
 int dirCount;
 int fileCount;
 int dTotalBytes;
 
+// Global variable for threshold
+float thresh;
+
 // results.json file
 FILE *fp;
+
+void listAllDirs(char *pPath);
+void readFile(char *filename);
 
 int main(int argc, char *argv[]){
 	
@@ -24,7 +31,7 @@ int main(int argc, char *argv[]){
 		exit(1);	// exit
 	}
 
-	float thresh = strtof(argv[2],NULL);	// Inputted threshold
+	thresh = strtof(argv[2],NULL);	// Inputted threshold
 	if(thresh < 0.0){	// Making sure threshold is positive
 		printf("Threshold cannot be negative\n");
 		exit(1);	// exit
@@ -44,7 +51,7 @@ int main(int argc, char *argv[]){
 	listAllDirs(pPath);
 
 	// Print dtextdetect results
-	printf("dtextdetect: Examined %i directories, %i files, and %i bytes from %s", dirCount, fileCount, dTotalBytes, filename);
+	printf("dtextdetect: Examined %i directories, %i files, and %i bytes from %s", dirCount, fileCount, dTotalBytes, pPath);
 
 	// Closing results.json file
 	if(fclose(fp) != 0){
@@ -55,9 +62,13 @@ int main(int argc, char *argv[]){
 }
 	
 void listAllDirs(char *pPath){
-
+	printf("pPath: %s\n",pPath);
 	DIR *dirp;
 	struct dirent *dp;
+	struct stat sb;
+	//char newPath[40] = "\0";
+	//strcpy(newPath, pPath);
+	//printf("newPath: %s\n",newPath);
 
 	dirp = opendir(pPath);
 
@@ -72,20 +83,36 @@ void listAllDirs(char *pPath){
 
 	// Loop using readdir until the result is NULL
 	while((dp = readdir(dirp)) != NULL){
-		if((dp = readdir(dirp)) != NULL){
-			
-			// Get file information		
-			stat(dp->d_name, &sb);
+		
+		// Get file information		
+		stat(dp->d_name, &sb);
 
-			if((sb.st_mode & S_IFMT) == S_ISDIR){
-				listAllDirs(dp->d_name);
-			}				
+		// Handle back directory
+		if( strcmp(dp->d_name,"..") == 0 || strcmp(dp->d_name,".") == 0 )
+			continue;
+
+		//if((sb.st_mode & S_IFMT) == S_ISDIR){
+		if( S_ISDIR(sb.st_mode) ){
+			dirCount++;
+			//printf("HERE");
+			//char *newPath = strcat(pPath,strcat("/",dp->d_name));
+			//strcat(pPath,"/");
+			//strcat(newPath, dp->d_name);
+			//strcat(newPath, "/");
+			//printf("New Path: %s\n", realpath(pPath));
+			listAllDirs(dp->d_name);
+			//printf("DIR\n");
+		}				
+		else{
+			fileCount++;
+			readFile(dp->d_name);
+			//printf("%s\n", dp->d_name);
 		}
 	}
 }
 
 	
-String readFile(char *filename){
+void readFile(char *filename){
 	
 	int fd = open(filename, O_RDONLY,0);
 
@@ -117,10 +144,10 @@ String readFile(char *filename){
 
 	// Outputting information to user
 	printf("textdetect: %s -> %.02f%% HR [%i / %i bytes]\n", filename, perc, readBytes, fTotalBytes);
-
+	
 	// Write statistics to results.json
 	if(perc >= thresh){
-		return ("[ {\"name\" : \"%s\", \"readable\" : %i, \"bytes\" : %i } ]\n", filename, readBytes, fTotalBytes);
+		fprintf(fp,"[ {\"name\" : \"%s\", \"readable\" : %i, \"bytes\" : %i } ]\n", filename, readBytes, fTotalBytes);
 	}
 		
 	// Closing file that was being read
