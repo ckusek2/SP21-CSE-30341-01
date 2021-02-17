951e6658a6c8197b02c08aa0843c103f9be3b197
Calvin Kusek <ckusek2@student13.cse.nd.edu>
2021-02-16 19:14:14 -0500

    Directory parsing

--- project1/dtextdetect.c
+++ project1/dtextdetect.c
@@ -7,12 +7,20 @@
 #include <string.h>
 #include <unistd.h>
 
+// Initialize directory, byte, and file counting variables
+int dirCount;
+int fileCount;
+int dTotalBytes;
+
+// results.json file
+FILE *fp;
+
 int main(int argc, char *argv[]){
 	
 	// Verify input
 	if(argc != 3){	// Making sure there is the correct number of args
 		printf("Incorrect number of arguments...\n");
-		printf("Usage: textdetect InputFile Threshold\n");
+		printf("Usage: dtextdetect SourcePath Threshold\n");
 		exit(1);	// exit
 	}
 
@@ -22,8 +30,63 @@ int main(int argc, char *argv[]){
 		exit(1);	// exit
 	}
 
-	// Opening file for reading
-	char *filename = argv[1];
+	// directory path
+	char *pPath = argv[1];
+
+	dirCount = 0;
+	fileCount = 0;
+	dTotalBytes = 0;
+
+	// Opening json file to write to
+	fp = fopen("results.json", "w+");
+
+	// Use listAllDirs to go through directories
+	listAllDirs(pPath);
+
+	// Print dtextdetect results
+	printf("dtextdetect: Examined %i directories, %i files, and %i bytes from %s", dirCount, fileCount, dTotalBytes, filename);
+
+	// Closing results.json file
+	if(fclose(fp) != 0){
+		printf("Unable to close results.json: %s\n", strerror(errno));
+		exit(1);
+	}
+
+}
+	
+void listAllDirs(char *pPath){
+
+	DIR *dirp;
+	struct dirent *dp;
+
+	dirp = opendir(pPath);
+
+	if(dirp == NULL){
+
+		char szTemp[256];
+
+		sprintf(szTemp, "Could not open %s", pPath);
+		perror(szTemp);
+		return;
+	}
+
+	// Loop using readdir until the result is NULL
+	while((dp = readdir(dirp)) != NULL){
+		if((dp = readdir(dirp)) != NULL){
+			
+			// Get file information		
+			stat(dp->d_name, &sb);
+
+			if((sb.st_mode & S_IFMT) == S_ISDIR){
+				listAllDirs(dp->d_name);
+			}				
+		}
+	}
+}
+
+	
+String readFile(char *filename){
+	
 	int fd = open(filename, O_RDONLY,0);
 
 	// Testing to make sure filename is an actual file
@@ -33,51 +96,33 @@ int main(int argc, char *argv[]){
 	}
 	
 	
-	// Going over file and counting human readable/nonreadable characters
-	
-	int dirCount = 0;
-	int fileCount = 0;
-	
+	// Going over file and counting human readable/nonreadable characters	
 	char buffer[4096];
-	int totalBytes = 0;	// Total number of bytes in file
+	int fTotalBytes = 0;	// Total number of bytes in file
 	int readBytes = 0;	// Number of human readable characters
-	
-	/* for all directories and files */
 
-		int result = read(fd, buffer, 4096);	// Read chunk of data from file
-		while(result > 0){	// Run while still data in file
-			for(int i = 0; i < result; i++){	// Count human readable bytes in chunk of data
-				if(buffer[i] >= 32 && buffer[i] <= 126){
-					readBytes++;
-				}
+	int result = read(fd, buffer, 4096);	// Read chunk of data from file
+	while(result > 0){	// Run while still data in file
+		for(int i = 0; i < result; i++){	// Count human readable bytes in chunk of data
+			if(buffer[i] >= 32 && buffer[i] <= 126){
+				readBytes++;
 			}
-			totalBytes += result;
-			result = read(fd, buffer, 4096);
 		}
-
-		float perc = (float)readBytes/(float)totalBytes * 100;	// % of human readable characters in file
-
-		// Outputting information to user
-		printf("textdetect: %s -> %.02f%% HR [%i / %i bytes]\n", filename, perc, readBytes, totalBytes);
-
-		// Opening json file to write to
-		FILE *fp = fopen("results.json", "w+");
-		// Write statistics to results.json
-		if(perc >= thresh){
-			fprintf(fp,"[ {\"name\" : \"%s\", \"readable\" : %i, \"bytes\" : %i } ]\n", filename, readBytes, totalBytes);
-		}
-
-	/* end loop */	
-
-	// Print dtextdetect results
-	printf("dtextdetect: Examined %i directories, %i files, and %i bytes from %s", dirCount, fileCount, totalBytes, filename);
-
-	// Closing results.json file
-	if(fclose(fp) != 0){
-		printf("Unable to close results.json: %s\n", strerror(errno));
-		exit(1);
+		fTotalBytes += result;
+		result = read(fd, buffer, 4096);
 	}
-	
+
+	float perc = (float)readBytes/(float)fTotalBytes * 100;	// % of human readable characters in file
+	dTotalBytes += fTotalBytes;	// Adding up bytes from all files in directory
+
+	// Outputting information to user
+	printf("textdetect: %s -> %.02f%% HR [%i / %i bytes]\n", filename, perc, readBytes, fTotalBytes);
+
+	// Write statistics to results.json
+	if(perc >= thresh){
+		return ("[ {\"name\" : \"%s\", \"readable\" : %i, \"bytes\" : %i } ]\n", filename, readBytes, fTotalBytes);
+	}
+		
 	// Closing file that was being read
 	if(close(fd) < 0){
 		printf("Unable to close %s: %s\n", filename, strerror(errno));
