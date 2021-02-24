885bd560b12c09b994089a95a8c1be29bbdb7ab1
Rafael Mendizabal <rmendiza@student13.cse.nd.edu>
2021-02-24 13:07:04 -0500

    created ndshell.c

---
+++ project2/ndshell.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]){
+	//TODO
+}
