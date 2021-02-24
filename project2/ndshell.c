bc91844d82059ea6bbf652a682f4b36bcd082618
Rafael Mendizabal <rmendiza@student13.cse.nd.edu>
2021-02-24 13:26:39 -0500

    Started looping inputs

--- project2/ndshell.c
+++ project2/ndshell.c
@@ -8,5 +8,16 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]){
-	//TODO
+	char command[10];
+	char parameter1[1000];
+	char parameter2[1000];
+	char parameter3[1000];
+	char parameter4[1000];
+
+	while(1){
+		printf("ndshell> ");
+		scanf("%s %s %s %s %s",command, parameter1, parameter2, parameter3, parameter4);
+		//printf("Entered %s with parameters %s, %s, %s, %s\n",command, parameter1, parameter2, parameter3, parameter4);
+		//TODO
+	}
 }
