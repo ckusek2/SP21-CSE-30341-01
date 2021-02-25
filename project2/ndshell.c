8af2fd8ebd66dd48d0f16cf7f4928c95a5fcb33b
Rafael Mendizabal <rmendiza@student13.cse.nd.edu>
2021-02-24 19:05:00 -0500

    Handle inputs

--- project2/ndshell.c
+++ project2/ndshell.c
@@ -7,17 +7,68 @@
 #include <string.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]){
-	char command[10];
-	char parameter1[1000];
-	char parameter2[1000];
-	char parameter3[1000];
-	char parameter4[1000];
+// Declare functions
+void commandHandler(char *command);
+void start(char *command);
+void wait(char *command);
+void waitfor(char *command);
+void run(char *command);
+void kill(char *command);
+void bound(char *command);
 
+int main(int argc, char *argv[]){
+	
+	char commandline[1000];
+	char *command;
+
+	// Loop over input
 	while(1){
 		printf("ndshell> ");
-		scanf("%s %s %s %s %s",command, parameter1, parameter2, parameter3, parameter4);
-		//printf("Entered %s with parameters %s, %s, %s, %s\n",command, parameter1, parameter2, parameter3, parameter4);
-		//TODO
+		scanf("%[^\n]%*c",commandline); // Read full line of input.
+		command = strtok(commandline, " "); // Split string into tokens. 
+		commandHandler(command);
 	}
+}
+
+void commandHandler(char *command){
+	
+	if(strcmp(command,"start") == 0)
+		start(command);
+	else if(strcmp(command,"wait") == 0)
+		wait(command);
+	else if(strcmp(command,"waitfor") == 0)
+		waitfor(command);
+	else if(strcmp(command,"run") == 0)
+		run(command);
+	else if(strcmp(command,"kill") == 0)
+		kill(command);
+	else if(strcmp(command,"bound") == 0)
+		bound(command);
+	else{
+		printf("ndshell: unknown command: %s\n", command);
+		return;
+	}
+	return;
+}
+
+void start(char *command){
+	command = strtok(NULL, " ");
+	printf("%s\n", command);
+	return;
+}
+void wait(char *command){
+	return;
+}
+void waitfor(char *command){
+	return;
+}
+void run(char *command){
+	
+	return;
+}
+void kill(char *command){
+	return;
+}
+void bound(char *command){
+	return;
 }
