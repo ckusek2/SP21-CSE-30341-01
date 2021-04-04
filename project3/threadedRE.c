d31f26c8fb326ac8bb8be88a9d8dbab7c3608314
Calvin Kusek <ckusek2@student13.cse.nd.edu>
2021-04-04 02:10:10 -0400

    input added

---
+++ project3/threadedRE.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+// Global variables
+int numCores = 8;	// need to find out optimal number of threads
+struct PacketHolder gBuffer[10];	// will be 10 or less files processed
+int gWriteSpot;
+int gReadSpot;
+// add hash table
+
+struct PacketHolder{
+	int theSize;
+	char *pPayload;
+	uint32_t hashValue;
+	int hitCount;
+};
+
+// Got this stuff from the panopto video from 5 days ago
+char PutInBuffer(struct PacketHolder ItemToPut);
+
+void* producerThread(void* pArgs){
+
+	// Open file and check it
+	
+	while(LoopToEndOfFile){	// If we use fgets, I think we can just say while fgets() != NULL
+
+		struct PacketHolder theHolder;
+		int nBytesRead;
+
+		// I believe we need a malloc here
+
+		nBytesRead = fread(pFile, theHolder->pPayload, ...);
+		theHolder.theSize = nBytesRead;
+
+		PutInBuffer(theHolder);
+	}
+}
+
+// Got this from the panopto video from 3 days ago
+// Hash value calculator
+uint32_t computeHash(char* pData, int nSize){
+
+	uint32_t hash = 0;
+	uint32_t *pBlock = (uint32_t *) (pData + 54); // I think pData is a byte array of the info in the packet
+	int nRemaining = nSize - 54;
+
+	while(nRemaining > 0){
+		hash = hash ^ *pBlock;	// xor hash and pBlock
+		pBlock = pBlock + 1;	// moves pBlock pointer over 4 bytes
+		nRemaining -= 4;
+
+		if(nRemaining < 4) // not 4 bytes to do xor with
+			break;
+	}
+
+	return hash;
+}
+
+// I think we also need to make a thread for input/output
+int main(int argc, char *argv[]){
+
+	int fileStart;	// Index in argv where names of files start
+	if(strcmp(argv[1], "-thread") == 0){
+		numCores = atoi(argv[2]);
+		fileStart = 3;	// file names start at argv[3]
+	} else
+		fileStart = 1;
+
+	struct stat stats;
+	for(fileStart; fileStart < argc; fileStart++){
+		stat(argv[fileStart], &stats);
+		// I think we can use stats.st_mode to make sure the file is a .pcap file
+		// If it is a pcap file, do stuff with it lol
+	}
+}
