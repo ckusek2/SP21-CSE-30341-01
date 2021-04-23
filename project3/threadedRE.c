5f0430f2af76fefb50d62b81998e05956c698815
Calvin Kusek <ckusek2@student13.cse.nd.edu>
2021-04-22 22:40:14 -0400

    just need to clean up errors

--- project3/threadedRE.c
+++ project3/threadedRE.c
@@ -25,25 +25,21 @@ struct PacketHolder{
 };
 
 // Global Variables
-int numThreads = 8;	// need to find out optimal number of threads
-struct PacketHolder gBuffer[10];
-int gWriteSpot = 0;
-int gReadSpot = 0;
+int numThreads = 3;	// starts at optimal number of threads
+struct PacketHolder gBuffer[10];	// buffer for passing packet data between producer and consumers
+int gIndex = 0;	// index the producer/consumers will be using to add to/take from buffer
 int skip = 54; // The # of bytes to be skipped 
+int fileStart;	// Index in argv where names of files start
+int fileEnd; // Index in argv where names of files end
 int numHits = 0;
 int doneProducing = 0;
+float totalBytes = 0;
+int totalUsedPackages = 0;
 pthread_mutex_t mutex;
 pthread_cond_t readFrom;
 pthread_cond_t putIn;
 
 // hash table
-struct DataItem {
-	uint32_t hash;
-	int key;
-};
-
-//struct DataItem* hashTable[256];
-
 uint32_t hashTable[256];
 
 // Hash value calculator
@@ -62,86 +58,101 @@ uint32_t computeHash(char* pData, int nSize){
 	return hash;
 }
 
+
 void PutInBuffer(struct PacketHolder ItemToPut){
-	gBuffer[gWriteSpot] = ItemToPut;
-	gWriteSpot = (gWriteSpot + 1) % 10;	// creates bounded buffer because doesn't allow gWriteSpot to go over 10
+	gBuffer[gIndex] = ItemToPut;
+	gIndex = gIndex + 1;	// creates bounded buffer because doesn't allow gWriteSpot to go over 10
 }
 
 void ReadFromBuffer(struct PacketHolder *ReadItem){ 
-	*ReadItem = gBuffer[gReadSpot];
-	gReadSpot = (gReadSpot + 1) % 10;	// same as for PutInBuffer, buffer has a size of 10 so can't go over that
+	*ReadItem = gBuffer[gIndex];
+	gIndex = gIndex - 1;	// same as for PutInBuffer, buffer has a size of 10 so can't go over that
 }
 
-void *producerThread(void* pArgs){
 
-	// Open file and check it
-	FILE *fp;
+void *producerThread(void* pArgs[]){
 
-	fp = fopen(pArgs, "r");
-
-	if(fp == NULL){
-		printf("Unable to open %c: %s\n", *(char*)pArgs, strerror(errno));
-		exit(1);	// exit
-	}
-
-	// Check magic number to make sure we are reading a pcap file
-	guint32 magicNumber;
-	fread((void*)&magicNumber, 4, 1, fp);
-	if(magicNumber == 0xd4c3b2a1){
-		printf("Not a valid pcap file \n");
-		exit(1);
-	}
-	
-	// This is how we read all the variables from the pcap global header	
-	guint32 variable;
-	for(int i = 0; i < 6; i++){
-		if(i == 0 || i == 1)
-			fread((void*)&variable, sizeof(guint16), 1, fp); // 16 bit major and minor numbers
-		else if(i == 2)
-			fread((void*)&variable, sizeof(gint32), 1, fp); // GMT to local correction
-		else
-			fread((void*)&variable, sizeof(guint32), 1, fp); // Other values
-
-		//printf("%x\n", variable); // Here for debuggin purposes
-	}
-	
-	// Read packets
-	while(!feof(fp)){	// Until EOF
-
-		guint32 ts_sec;
-		guint32 ts_usec;
-		guint32 incl_len;
-		guint32 origin_len;
+	for(int i = fileStart; i < fileEnd; i++){
 		
-		fread((void*)&ts_sec, 4, 1, fp);
-		fread((void*)&ts_usec, 4, 1, fp);
-		fread((void*)&incl_len, 4, 1, fp);
-		fread((void*)&origin_len, 4, 1, fp);
-
-			
-		struct PacketHolder theHolder;
-
-		theHolder.theSize = incl_len;
-		theHolder.pPayload = malloc(theHolder.theSize);
-
-		// Read and store the packet contents
-		fread(theHolder.pPayload, 1, theHolder.theSize, fp);
-		
-		if(theHolder.theSize >= 128){
-			pthread_mutex_lock(&mutex);	// lock mutex
-			pthread_cond_wait(&readFrom, &mutex);	// wait to see if consumer thread is reading from the buffer
-			PutInBuffer(theHolder);	// put theHolder in the bounded buffer
-			pthread_cond_broadcast(&putIn);	// broadcast to all consumer threads that theHolder has been added to the buffer
-			pthread_mutex_unlock(&mutex);	// unlock mutex
+		char *inputFile = pArgs[i];
+		struct stat stats;
+		int status = stat(inputFile, &stats);
+		if(status < 0){ // Make sure the file exists
+			printf("%s does not exist. \n", inputFile);
+			exit(0);
 		}
+		if(!(strstr(inputFile, ".pcap"))) {	// Make sure the file is a pcap file
+			printf("%s is not a .pcap file\n", inputFile);
+			exit(0);
+		}
+		
+		// Open and check file
+		FILE *fp;
+		fp = fopen(inputFile, "r");
+		if(fp == NULL){
+			printf("Unable to open %c: %s\n", *(char*)inputFile, strerror(errno));
+			exit(1);	// exit
+		}
+
+		// Check magic number to make sure we are reading a pcap file
+		guint32 magicNumber;
+		fread((void*)&magicNumber, 4, 1, fp);
+		if(magicNumber == 0xd4c3b2a1){
+			printf("Not a valid pcap file \n");
+			exit(1);
+		}
+	
+		// This is how we read all the variables from the pcap global header	
+		guint32 variable;
+		for(int i = 0; i < 6; i++){
+			if(i == 0 || i == 1)
+				fread((void*)&variable, sizeof(guint16), 1, fp); // 16 bit major and minor numbers
+			else if(i == 2)
+				fread((void*)&variable, sizeof(gint32), 1, fp); // GMT to local correction
+			else
+				fread((void*)&variable, sizeof(guint32), 1, fp); // Other values
+		}
+	
+		// Read packets
+		while(!feof(fp)){	// Until EOF
+
+			guint32 ts_sec;
+			guint32 ts_usec;
+			guint32 incl_len;
+			guint32 origin_len;
+		
+			fread((void*)&ts_sec, 4, 1, fp);
+			fread((void*)&ts_usec, 4, 1, fp);
+			fread((void*)&incl_len, 4, 1, fp);
+			fread((void*)&origin_len, 4, 1, fp);
+
+			struct PacketHolder theHolder;
+
+			theHolder.theSize = incl_len;
+			totalBytes += incl_len;
+			theHolder.pPayload = malloc(theHolder.theSize);
+
+			// Read and store the packet contents
+			fread(theHolder.pPayload, 1, theHolder.theSize, fp);
+	
+			if(theHolder.theSize >= 128){
+				pthread_mutex_lock(&mutex);	// lock mutex
+				if(gIndex == 10)
+					pthread_cond_wait(&readFrom, &mutex);	// wait to see if consumer thread is reading from the buffer
+				totalUsedPackages += 1;
+				PutInBuffer(theHolder);	// put theHolder in the bounded buffer
+				pthread_cond_signal(&putIn);	// broadcast to all consumer threads that theHolder has been added to the buffer
+				pthread_mutex_unlock(&mutex);	// unlock mutex
+			}
+		}
+
+		// Closing file that was being read
+		fclose(fp);
 	}
-
-	doneProducing = 1;
-
-	// Closing file that was being read
-	fclose(fp);
-
+	doneProducing = 1;	
+	return NULL;
 }
+
 
 void *consumerThread(){
 
@@ -149,9 +160,9 @@ void *consumerThread(){
 		struct PacketHolder theHolder;
 
 		pthread_mutex_lock(&mutex);	// lock mutex
-		pthread_cond_wait(&putIn, &mutex);	// wait to see if the producer thread is putting anything in buffer
+		while(gIndex == 0)
+			pthread_cond_wait(&putIn, &mutex);	// wait to see if the producer thread is putting anything in buffer
 		ReadFromBuffer(&theHolder);	// read a PacketHolder from the bounded buffer, pass by reference to update theHolder
-	
 		uint32_t hash = computeHash(theHolder.pPayload, theHolder.theSize);
 		int key = hash % 256;
 		if(hashTable[key] == 0)
@@ -163,34 +174,54 @@ void *consumerThread(){
 		pthread_cond_signal(&readFrom);	// signal to the producer thread that something has been taken from the buffer
 		pthread_mutex_unlock(&mutex);	// unlock mutex
 	}
+
+	return NULL;
 }
+
 
 int main(int argc, char *argv[]){
 
-	printf("Welcome to Project 3 - ThreadRE by Calvin Kusek, Dalton Dove, and Rafael Mendizabal \n");
+	printf("Welcome to Project 3 - ThreadRE by Calvin Kusek, Dalton Dove, and Rafael Mendizabal\n");
+	printf("Now operating in Level 1 mode - full payload detection\n");
 
-	int fileStart;	// Index in argv where names of files start
 	if(strcmp(argv[1], "-thread") == 0){
 		numThreads = atoi(argv[2]);
-		fileStart = 3;	// file names start at argv[3]
-	} else
+		if(numThreads < 2){
+			printf("Minimum of two threads required \n");
+			exit(0);
+		} else if(numThreads > 8){
+			printf("Cannot use more than 8 threads/cores\n");
+			exit(0);
+		} else {
+			printf("Threads allowed: %i\n", numThreads);
+			fileStart = 3;	// file names start at argv[3]
+		}
+	} else {
+		printf("Using optimal number of threads: %i\n", numThreads);
 		fileStart = 1; // file names start at argv[1]
+	}
+
+	printf("Allocating 1 thread to file I/O, %i thread(s) to processing\n", numThreads - 1);
+	printf("Files to process: ");
+	for(int output = fileStart; output < argc; output++)
+		printf("%s ", argv[output]);
+	printf("\n");
+	fileEnd = argc;
 
 	for(int i = 0; i < 256; i++)
 		hashTable[i] = 0;
 
-	struct stat stats;
-	int status;
 	pthread_t producer;
 	pthread_t consumer;
-	for(int start = fileStart; start < argc; start++){
-		char *inputFile = argv[start];
-		status = stat(inputFile, &stats);
-		if(status < 0) // Make sure the file exists
-			printf("%s does not exist. \n", inputFile);
-		if(strstr(inputFile, ".pcap")) {	// Make sure the file is a pcap file
-			pthread_create(&producer, NULL, &producerThread, inputFile);
-			pthread_create(&consumer, NULL, &consumerThread, NULL);
-		}
-	}
+	pthread_create(&producer, NULL, &producerThread, argv);
+	for(int i = 0; i < numThreads - 1; i++)
+		pthread_create(&consumer, NULL, &consumerThread, NULL);
+	pthread_join(producer, NULL);
+
+	printf("Results:\n");
+	printf("%f MB processed\n", totalBytes / 1000000);
+	printf("%i hits\n", numHits);
+	printf("%i packages used\n", totalUsedPackages);
+	float redundancy = ((float)numHits / totalUsedPackages) * 100;
+	printf("%.2f%c redundancy detected\n", redundancy, '%');
 }
