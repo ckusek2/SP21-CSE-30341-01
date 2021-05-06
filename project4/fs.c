783511ffa6f25b24be3778bba5fdd6aa79e21eae
Calvin Kusek <ckusek2@student13.cse.nd.edu>
2021-05-06 00:26:34 -0400

    fs_debug complete

--- project4/fs.c
+++ project4/fs.c
@@ -44,7 +44,9 @@ void fs_debug()
 	// declaring unions
 	union fs_block block;
 	union fs_block iNodeBlock;
+	union fs_block indirectBlock;
 
+	// load disk block 0
 	disk_read(0,block.data);
 
 	// printing superblock information
@@ -55,13 +57,17 @@ void fs_debug()
 	printf("    %d blocks for inodes\n",block.super.ninodeblocks);
 	printf("    %d inodes total\n",block.super.ninodes);
 
+	// load disk block 1
 	disk_read(1, iNodeBlock.data);
 
 	for(int i = 0; i < POINTERS_PER_INODE; i++){
+		// make sure inode is valid
 		if(iNodeBlock.inode[i].isvalid){
+			// print inode number and size
 			printf("inode %d:\n", i);
 			printf("    size: %d bytes\n", iNodeBlock.inode[i].size);
 			
+			// print out the direct blocks in iNodeBlock
 			printf("    direct blocks: ");
 			for(int j = 0; j < POINTERS_PER_INODE; j++){
 				if(iNodeBlock.inode[i].direct[j])
@@ -69,10 +75,23 @@ void fs_debug()
 			}
 			printf("\n");
 
+
 			int indirect = iNodeBlock.inode[i].indirect;
+			// check if indirect block is 0
 			if(indirect){
+				// print indirect block number
 				printf("    indirect block: %d\n", iNodeBlock.inode[i].indirect);
 
+				// print indirect data blocks
+				printf("    indirect data blocks: ");
+				// load disk block of indirect disk
+				disk_read(indirect, indirectBlock.data);
+				// go through pointers on indirect block and print non-zero pointer values
+				for(int pointer = 0; pointer < POINTERS_PER_BLOCK; pointer++){
+					if(indirectBlock.pointers[pointer])
+						printf("%d ", indirectBlock.pointers[pointer]);
+				}
+				printf("\n");
 			}
 		}
 	}
